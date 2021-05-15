db965582fd73a277bd121c04c32a84bca7004bc3
Fabio-Colombo <thesmallworld95@gmail.com>
2021-05-15 18:14:44 +0200

    Layout Homepage Completo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,68 @@
 <footer>
-  
+  <section class="link-background">
+    <div class="link-container pt-10 pb-6">
+      <div class="link">
+        <h3>dc comics</h3>
+        <ul>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movie</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+        </ul>
+      </div>
+      <div class="link pl-10">
+        <h3>dc</h3>
+        <ul>
+          <li>Terms Of Use</li>
+          <li>Privacy policy (New)</li>
+          <li>Ad Choises</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div class="link">
+        <h3>sites</h3>
+        <ul>
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>DC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+      <div class="link shop">
+        <h3>shop</h3>
+        <ul>
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
+        </ul>
+      </div>
+    </div>
+    <div class="copyright pb-6">
+      <p>All Site Content TM and &copy 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. </p>
+      <a href="#">Cookies Settings</a>
+    </div>
+  </section>
+  <section class="social">
+    <div class="social-container pt-10 pb-10">
+      <button class="text-uppercase text-white" type="button" name="button">Sign-up now!</button>
+      <div class="">
+        <span class="text-uppercase pr-6">follow us</span>
+        <img src="/img/footer-facebook.png" alt="">
+        <img src="/img/footer-twitter.png" alt="">
+        <img src="/img/footer-youtube.png" alt="">
+        <img src="/img/footer-pinterest.png" alt="">
+        <img src="/img/footer-periscope.png" alt="">
+      </div>
+    </div>
+  </section>
 </footer>
